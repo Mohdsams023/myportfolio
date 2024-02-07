986302404f4300be59dcaad6e86991fd7ed7cfd7
mohdsams23 <157204613+Mohdsams023@users.noreply.github.com>
2024-02-07 16:40:34 +0530

    done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+});
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
